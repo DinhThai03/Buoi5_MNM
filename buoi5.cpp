19f8569fa2f59a34f99e1da37d3c570d055082bc
DinhThai03 <thai.05.10.2001@gmail.com>
2024-11-09 10:33:02 +0700

    Khoi tao kho

---
+++ buoi5.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+bool soHoanThien(int a);
+bool soNguyeTo(int a);
+bool soChinhPhuong(int a);
+bool soChan(int a);
+
+int main()
+{
+	int a;
+	cout << "Nhap so can kiem tra: ";
+	cin >> a;
+
+	//mau
+	//if (soHoanThien(a))
+	//	cout << a << " la so hoan thien";
+	//else
+	//	cout << a << " khong la so hoan thien";
+
+}
