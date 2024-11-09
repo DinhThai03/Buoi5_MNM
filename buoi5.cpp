891a534471afdf1a1c68d7cf67f260caa048710c
Griffania <phankhang300603@gmail.com>
2024-11-09 11:57:09 +0700

    sochinhphuong

--- buoi5.cpp
+++ buoi5.cpp
@@ -2,58 +2,21 @@
 
 using namespace std;
 
-bool soNguyeTo(int a)
-{
-	if(a < 2)
-		return false;
-	for(int i =0 ; i <= sqrt(a); i++)
-		if(a%i == 0)
-			return false;
-	return true;
-	
+bool soNguyeTo(int a);
+bool soHoanThien(int a);
+bool soChinhPhuong(int a){
+	int temp = sqrt(a);
+	if(temp*temp==a)
+		return true;
+	return false;
 }
-
-bool soHoanThien(int a){
-	 if (n <= 1) return false;  // Các số nhỏ hơn hoặc bằng 1 không phải là số hoàn hảo
-    
-    int sum = 0;
-    
-    // Tính tổng các ước của số n (ngoại trừ chính nó)
-    for (int i = 1; i <= n / 2; ++i) {
-        if (n % i == 0) {  // Nếu i là ước của n
-            sum += i;
-        }
-    }
-    
-    // Kiểm tra nếu tổng các ước bằng chính số n
-    return sum == n;
-};
-
-
-bool soChinhPhuong(int a)
-{
-	//day la nhanh moi
-}
-
-//comment
-bool soChinhPhuong(int a);
->>>>>>> 9fe563f (commnet)
 bool soChan(int a);
-
 int main()
 {
-	int a;
-	cout << "Nhap so can kiem tra: ";
-	cin >> a;
-
-	if (soNguyeTo(a))
-		cout << a << " la so nguyen to";
+	
+	if (soChinhPhuong(a))
+		cout << a << " la so chinh phuong";
 	else
-		cout << a << " khong la so nguyen to";
-
-	if (sohoanthien(a))
-		cout << a << " la so hoan thien";
-	else
-		cout << a << " khong la so hoan thien";
+		cout << a << " khong la so chinh phuong";
 
 }
