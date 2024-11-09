a65b55077b1c7c8e88ff66cc71757505a3a206fc
YasuoTri <phamminhtri26102003@gmail.com>
2024-11-09 10:43:19 +0700

    sohoanthien

--- buoi5.cpp
+++ buoi5.cpp
@@ -2,7 +2,21 @@
 
 using namespace std;
 
-bool soHoanThien(int a);
+bool soHoanThien(int a){
+	 if (n <= 1) return false;  // Các số nhỏ hơn hoặc bằng 1 không phải là số hoàn hảo
+    
+    int sum = 0;
+    
+    // Tính tổng các ước của số n (ngoại trừ chính nó)
+    for (int i = 1; i <= n / 2; ++i) {
+        if (n % i == 0) {  // Nếu i là ước của n
+            sum += i;
+        }
+    }
+    
+    // Kiểm tra nếu tổng các ước bằng chính số n
+    return sum == n;
+};
 bool soNguyeTo(int a);
 bool soChinhPhuong(int a);
 bool soChan(int a);
@@ -13,10 +27,10 @@ int main()
 	cout << "Nhap so can kiem tra: ";
 	cin >> a;
 
-	//mau
-	//if (soHoanThien(a))
-	//	cout << a << " la so hoan thien";
-	//else
-	//	cout << a << " khong la so hoan thien";
+
+if (sohoanthien(a))
+	cout << a << " la so hoan thien";
+else
+	cout << a << " khong la so hoan thien";
 
 }
